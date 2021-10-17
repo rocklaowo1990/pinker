600361856eb9450cc33f0289f9a378d54c646153
Macbook Pro <macbookpro@MacbookdeMacBook-Pro.local>
2021-10-17 16:59:59 +0800

    优化

--- lib/pages/frame/register/register_view.dart
+++ lib/pages/frame/register/register_view.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
-import 'package:pinker/lang/translation_service.dart';
 
+import 'package:pinker/lang/translation_service.dart';
 import 'package:pinker/pages/frame/register/controller.dart';
 import 'package:pinker/values/values.dart';
 import 'package:pinker/widgets/widgets.dart';
