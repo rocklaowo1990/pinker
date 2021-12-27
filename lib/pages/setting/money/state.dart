7741a7b1bec60cb13d9316deb0251308abf4efe5
Macbook Pro <macbookpro@MacbookdeMacBook-Pro.local>
2021-12-28 00:57:51 +0800

    money

--- lib/pages/setting/money/state.dart
+++ lib/pages/setting/money/state.dart
@@ -6,11 +6,146 @@ class MoneyState {
   set huId(int value) => _huId.value = value;
   int get huId => _huId.value;
 
-  final RxList<int> _resault = [-20, 0, 200, 0].obs;
+  final RxList<int> _resault = [0, 0, 0, 0].obs;
   set resault(List<int> value) => _resault.value = value;
   List<int> get resault => _resault;
 
-  final RxList<Map<dynamic, dynamic>> _play = [{}, {}, {}, {}].obs;
-  set play(List<Map<dynamic, dynamic>> value) => _play.value = value;
-  List<Map<dynamic, dynamic>> get play => _play;
+  final RxList<int> _resaultOnly = [0, 0, 0, 0].obs;
+  set resaultOnly(List<int> value) => _resaultOnly.value = value;
+  List<int> get resaultOnly => _resaultOnly;
+
+  final RxList<int> _public = [0, 0, 0, 0].obs;
+  set public(List<int> value) => _public.value = value;
+  List<int> get public => _public;
+
+  final RxList<List<int>> _ma = [
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0]
+  ].obs;
+  set ma(List<List<int>> value) => _ma.value = value;
+  List<List<int>> get ma => _ma;
+
+  final RxList<List<Map<String, int>>> _payOnly = [
+    [
+      {
+        'id': 1,
+        'number': 0,
+      },
+      {
+        'id': 2,
+        'number': 0,
+      },
+      {
+        'id': 3,
+        'number': 0,
+      }
+    ],
+    [
+      {
+        'id': 0,
+        'number': 0,
+      },
+      {
+        'id': 2,
+        'number': 0,
+      },
+      {
+        'id': 3,
+        'number': 0,
+      }
+    ],
+    [
+      {
+        'id': 0,
+        'number': 0,
+      },
+      {
+        'id': 1,
+        'number': 0,
+      },
+      {
+        'id': 3,
+        'number': 0,
+      }
+    ],
+    [
+      {
+        'id': 0,
+        'number': 0,
+      },
+      {
+        'id': 1,
+        'number': 0,
+      },
+      {
+        'id': 2,
+        'number': 0,
+      }
+    ],
+  ].obs;
+  set payOnly(List<List<Map<String, int>>> value) => _payOnly.value = value;
+  List<List<Map<String, int>>> get payOnly => _payOnly;
+
+  final RxList<List<Map<String, int>>> _shouOnly = [
+    [
+      {
+        'id': 1,
+        'number': 0,
+      },
+      {
+        'id': 2,
+        'number': 0,
+      },
+      {
+        'id': 3,
+        'number': 0,
+      }
+    ],
+    [
+      {
+        'id': 0,
+        'number': 0,
+      },
+      {
+        'id': 2,
+        'number': 0,
+      },
+      {
+        'id': 3,
+        'number': 0,
+      }
+    ],
+    [
+      {
+        'id': 0,
+        'number': 0,
+      },
+      {
+        'id': 1,
+        'number': 0,
+      },
+      {
+        'id': 3,
+        'number': 0,
+      }
+    ],
+    [
+      {
+        'id': 0,
+        'number': 0,
+      },
+      {
+        'id': 1,
+        'number': 0,
+      },
+      {
+        'id': 2,
+        'number': 0,
+      }
+    ],
+  ].obs;
+  set shouOnly(List<List<Map<String, int>>> value) => _shouOnly.value = value;
+  List<List<Map<String, int>>> get shouOnly => _shouOnly;
 }
--- lib/pages/setting/money/view.dart
+++ lib/pages/setting/money/view.dart
@@ -13,29 +13,24 @@ class MoneyView extends GetView<MoneyController> {
 
   @override
   Widget build(BuildContext context) {
-    /// appBar 右侧的设置按钮
-    Widget settingBox = getButton(
-      child: SvgPicture.asset('assets/svg/icon_setting.svg'),
-      background: Colors.transparent,
-      width: 30.w,
-      height: 30.w,
-    );
-
     /// appBar
-    AppBar appBar = getAppBar(getSpan('麻将结算系统'),
-        line: AppColors.line,
-        backgroundColor: AppColors.secondBacground,
-        actions: [settingBox]);
+    AppBar appBar = getAppBar(
+      getSpan('麻将结算系统'),
+      line: AppColors.line,
+      backgroundColor: AppColors.secondBacground,
+    );
 
     Widget playerBox({
       required int id,
     }) {
-      return Container(
+      return getButton(
         width: double.infinity,
         padding: EdgeInsets.all(9.w),
-        decoration: BoxDecoration(
-            color: AppColors.secondBacground,
-            borderRadius: BorderRadius.all(Radius.circular(8.w))),
+        background: AppColors.secondBacground,
+        borderRadius: BorderRadius.all(Radius.circular(8.w)),
+        onPressed: () {
+          controller.handleSet(id);
+        },
         child: Column(
           children: [
             Row(
@@ -47,8 +42,9 @@ class MoneyView extends GetView<MoneyController> {
                       'assets/svg/avatar_default.svg',
                       height: 30,
                     ),
-                    SizedBox(width: 10.w),
-                    getSpan('玩家：${id + 1}'),
+                    SizedBox(width: 6.w),
+                    Obx(() => getSpan(
+                        '玩家 ${id + 1}  (单：${controller.state.resaultOnly[id]})')),
                   ],
                 ),
                 Obx(() => controller.state.resault[id] == 0
@@ -60,18 +56,57 @@ class MoneyView extends GetView<MoneyController> {
                             fontSize: 20, color: Colors.green)),
               ],
             ),
-            SizedBox(height: 10.h),
-            Obx(() => controller.state.play[id].isEmpty
-                ? getButton(
-                    child: getSpan('请为玩家添加参数'),
-                    width: double.infinity,
-                    background: AppColors.line,
-                    height: 22.h,
-                    onPressed: () {
-                      controller.handleSet(id);
-                    },
-                  )
-                : Row()),
+            SizedBox(height: 8.h),
+            Container(
+              width: double.infinity,
+              height: 1,
+              color: AppColors.line,
+            ),
+            SizedBox(height: 6.h),
+            Obx(() => Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        getSpan('所有人要给你的数量：', color: AppColors.secondText),
+                        getSpan(
+                          '${controller.state.public[id]}',
+                          color: AppColors.secondText,
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        getSpan('你的单独支出：', color: AppColors.secondText),
+                        getSpan(
+                          '${controller.state.payOnly[id][0]['id']! + 1}(${controller.state.payOnly[id][0]['number']}), ${controller.state.payOnly[id][1]['id']! + 1}(${controller.state.payOnly[id][2]['number']}), ${controller.state.payOnly[id][2]['id']! + 1}(${controller.state.payOnly[id][2]['number']})',
+                          color: AppColors.secondText,
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        getSpan('你的单独收入：', color: AppColors.secondText),
+                        getSpan(
+                          '${controller.state.shouOnly[id][0]['id']! + 1}(${controller.state.shouOnly[id][0]['number']}), ${controller.state.shouOnly[id][1]['id']! + 1}(${controller.state.shouOnly[id][2]['number']}), ${controller.state.shouOnly[id][2]['id']! + 1}(${controller.state.shouOnly[id][2]['number']})',
+                          color: AppColors.secondText,
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        getSpan('你中的码：', color: AppColors.secondText),
+                        getSpan(
+                          '1(${controller.state.ma[id][0]}), 2(${controller.state.ma[id][1]}), 3(${controller.state.ma[id][2]}), 4(${controller.state.ma[id][3]})',
+                          color: AppColors.secondText,
+                        ),
+                      ],
+                    ),
+                  ],
+                )),
           ],
         ),
       );
